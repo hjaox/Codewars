7b4c0afa888d1b0fed75ce6377d2fc00fee52c00
Hector <hectorjana.o@gmail.com>
2023-11-03 15:58:39 +0000

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CSharpUnitTests/IsItANumberTest.cs
@@ -0,0 +1,15 @@
+using CSharp;
+
+namespace CSharpUnitTests
+{
+    [TestFixture]
+    public class IsItANumberTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(IsItANumber.IsDigit("s2324"), Is.EqualTo(false));
+            Assert.That(IsItANumber.IsDigit("-234.4"), Is.EqualTo(true));
+        }
+    }
+}
