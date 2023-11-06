f3ccb4d4b70a416a0c4166287cae899143d54d51
Hector <hectorjana.o@gmail.com>
2023-11-06 22:44:55 +0000

    added FilterOutTheGeese kata

---
+++ CSharpUnitTests/FilterOutTheGeeseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CSharp;
+using NUnit.Framework.Interfaces;
+
+namespace CSharpUnitTests
+{
+    [TestFixture]
+    public class FilterOutTheGeeseTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            string[] testInput = { "Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish" };
+            string[] expected = { "Mallard", "Hook Bill", "Crested", "Blue Swedish" };
+
+            Assert.That(FilterOutTheGeese.GooseFilter(testInput), Is.EqualTo(expected));
+
+            testInput = new string[] { "Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested" };
+            expected = new string[] { "Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested" };
+
+            Assert.That(FilterOutTheGeese.GooseFilter(testInput), Is.EqualTo(expected));
+
+            testInput = new string[] { "African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher" };
+            expected = new string[] { };
+
+            Assert.That(FilterOutTheGeese.GooseFilter(testInput), Is.EqualTo(expected));
+        } 
+    }
+}
