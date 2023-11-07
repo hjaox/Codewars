bdd4b3d2bd229baec49ae447b8e901b9233a27d5
Hector <hectorjana.o@gmail.com>
2023-11-07 15:12:52 +0000

    added RegexBasicsIsItADigit kata

---
+++ CSharp/8kyu/RegexpBasicsIsItADigit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSharp
+{
+    public static class RegexpBasicsIsItADigit
+    {
+        public static bool Digit(this string str)
+        {
+            return Regex.IsMatch(str, @"^\d\z");
+        }
+    }
+}
