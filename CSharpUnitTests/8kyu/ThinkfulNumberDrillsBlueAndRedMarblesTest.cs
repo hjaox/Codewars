93a64c2d3cccd1caa95b915ae8d17d1d87d0d6ef
Hector <hectorjana.o@gmail.com>
2023-11-07 10:26:46 +0000

    added ThinkfulNumberDrillsBlueAndRedMarbles kata

---
+++ CSharpUnitTests/8kyu/ThinkfulNumberDrillsBlueAndRedMarblesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp;
+
+namespace CSharpUnitTests
+{
+    [TestFixture]
+    public class ThinkfulNumberDrillsBlueAndRedMarblesTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(ThinkfulNumberDrillsBlueAndRedMarbles.GuessBlue(5, 5, 2, 3), Is.EqualTo(0.6));
+            Assert.That(ThinkfulNumberDrillsBlueAndRedMarbles.GuessBlue(5, 7, 4, 3), Is.EqualTo(0.2));
+            Assert.That(ThinkfulNumberDrillsBlueAndRedMarbles.GuessBlue(12, 18, 4, 6), Is.EqualTo(0.4));
+        }
+    }
+}
