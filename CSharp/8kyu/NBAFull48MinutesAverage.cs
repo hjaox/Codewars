80c6d1d8520a551e8e91eb6ae6b795d98d0e0046
Hector <hectorjana.o@gmail.com>
2023-11-07 19:42:26 +0000

    added NBAFull48MinutesAverage kata

---
+++ CSharp/8kyu/NBAFull48MinutesAverage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+https://www.codewars.com/kata/587c2d08bb65b5e8040004fd/
+
+An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+
+Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+
+Examples:
+
+Kata.NbaExtrap(12, 20) => 28.8
+Kata.NbaExtrap(10, 10) => 48
+Kata.NbaExtrap(5, 17)  => 14.1
+Kata.NbaExtrap(0, 0)   => 0
+Notes:
+All inputs will be either be an integer or float.
+Follow your dreams!
+*/
+
+namespace CSharp
+{
+    public class NBAFull48MinutesAverage
+    {
+        public static double NbaExtrap(double ppg, double mpg)
+        {
+            return ppg > 0 ? Math.Round((ppg / mpg) * 48.0, 1) : 0;
+        }
+    }
+}
---
+++ CSharpUnitTests/8kyu/NBAFull48MinutesAverageTest.cs
@@ -0,0 +1,25 @@
+﻿using CSharp;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpUnitTests
+{
+    [TestFixture]
+    public class NBAFull48MinutesAverageTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            ClassicAssert.AreEqual(28.8, NBAFull48MinutesAverage.NbaExtrap(12, 20));
+            ClassicAssert.AreEqual(48.0, NBAFull48MinutesAverage.NbaExtrap(10, 10));
+            ClassicAssert.AreEqual(14.1, NBAFull48MinutesAverage.NbaExtrap(5, 17));
+            ClassicAssert.AreEqual(0, NBAFull48MinutesAverage.NbaExtrap(0, 0));
+            ClassicAssert.AreEqual(42.6, NBAFull48MinutesAverage.NbaExtrap(30.8, 34.7));
+            ClassicAssert.AreEqual(32.5, NBAFull48MinutesAverage.NbaExtrap(22.9, 33.8));
+        }
+    }
+}
