bba8f61b0998a4594aa393e4881a6d587b06e1e9
Hector <hectorjana.o@gmail.com>
2023-11-07 19:44:29 +0000

    added more info in previous katas

--- CSharp/8kyu/RegexpBasicsIsItADigit.cs
+++ CSharp/8kyu/RegexpBasicsIsItADigit.cs
@@ -2,6 +2,12 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
+/*
+https://www.codewars.com/kata/567bf4f7ee34510f69000032
+
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+*/
+
 namespace CSharp
 {
     public static class RegexpBasicsIsItADigit
