c27d5a381bf8d442bc484443c5cb6cce11feab0a
Hector <hectorjana.o@gmail.com>
2023-11-08 09:04:01 +0000

    added AreaOrPerimeter kata

---
+++ CSharpUnitTests/8kyu/AreaOrPerimeterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp;
+
+namespace CSharpUnitTests
+{
+    [TestFixture]
+    public class AreaOrPerimeterTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            ClassicAssert.AreEqual(AreaOrPerimeter.AreaOrPerimeterFunc(4,4), 16);
+            ClassicAssert.AreEqual(AreaOrPerimeter.AreaOrPerimeterFunc(6, 10), 32);
+        }
+    }
+}
