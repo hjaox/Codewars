883bcaccc26927d3f80b4ca7a1abe7bfb9b378b1
Hector <hectorjana.o@gmail.com>
2023-11-08 20:34:50 +0000

    added CheckSameCase kata

---
+++ CSharp/8kyu/CheckSameCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+https://www.codewars.com/kata/5dd462a573ee6d0014ce715b/csharp
+
+
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+*/
+
+namespace CSharp._8kyu
+{
+    public class CheckSameCase
+    {
+        public static int SameCase(char a, char b)
+        {
+            if (!Char.IsLetter(a) || !Char.IsLetter(b)) return -1;
+
+            return Char.IsUpper(a) == Char.IsUpper(b) ? 1 : 0;
+        }
+    }
+}
---
+++ CSharpUnitTests/8kyu/CheckSameCaseTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests._8kyu
+{
+    [TestFixture]
+    public class CheckSameCaseTest
+    {
+        [Test]
+        public void TrueTests()
+        {
+            ClassicAssert.AreEqual(1, CheckSameCase.SameCase('a', 'u'));
+            ClassicAssert.AreEqual(1, CheckSameCase.SameCase('A', 'U'));
+            ClassicAssert.AreEqual(1, CheckSameCase.SameCase('Q', 'P'));
+            ClassicAssert.AreEqual(1, CheckSameCase.SameCase('w', 'y'));
+            ClassicAssert.AreEqual(1, CheckSameCase.SameCase('c', 'm'));
+            ClassicAssert.AreEqual(1, CheckSameCase.SameCase('N', 'W'));
+        }
+
+        [Test]
+        public void FalseTests()
+        {
+            ClassicAssert.AreEqual(0, CheckSameCase.SameCase('a', 'U'));
+            ClassicAssert.AreEqual(0, CheckSameCase.SameCase('A', 'u'));
+            ClassicAssert.AreEqual(0, CheckSameCase.SameCase('Q', 'p'));
+            ClassicAssert.AreEqual(0, CheckSameCase.SameCase('w', 'Y'));
+            ClassicAssert.AreEqual(0, CheckSameCase.SameCase('c', 'M'));
+            ClassicAssert.AreEqual(0, CheckSameCase.SameCase('N', 'w'));
+        }   
+            
+        [Test]
+        public void NotLetters()
+        {   
+            ClassicAssert.AreEqual(-1, CheckSameCase.SameCase('a', '*'));
+            ClassicAssert.AreEqual(-1, CheckSameCase.SameCase('A', '%'));
+            ClassicAssert.AreEqual(-1, CheckSameCase.SameCase('Q', '1'));
+            ClassicAssert.AreEqual(-1, CheckSameCase.SameCase('w', '-'));
+            ClassicAssert.AreEqual(-1, CheckSameCase.SameCase('c', '8'));
+            ClassicAssert.AreEqual(-1, CheckSameCase.SameCase('N', ':'));
+        }
+    }
+}
