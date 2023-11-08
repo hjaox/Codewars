9cc8feeba853cd0effc716217cb15e7ab94ea954
Hector <hectorjana.o@gmail.com>
2023-11-08 19:45:36 +0000

    added FuelCalculatorTotalCost kata

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp._8kyu;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ CSharpUnitTests/8kyu/FuelCalculatorTotalCostTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests
+{
+    [TestFixture]
+    public class FuelCalculatorTotalCostTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(FuelCalculatorTotalCost.FuelPrice(5, 1.23), Is.EqualTo(5.65));
+            Assert.That(FuelCalculatorTotalCost.FuelPrice(8, 2.5), Is.EqualTo(18.40));
+            Assert.That(FuelCalculatorTotalCost.FuelPrice(5, 5.6), Is.EqualTo(27.50));
+        }
+
+        [Test]
+        public void AdditionalTests()
+        {
+            Assert.That(FuelCalculatorTotalCost.FuelPrice(169, 12.71), Is.EqualTo(2105.74));
+            Assert.That(FuelCalculatorTotalCost.FuelPrice(7, 35.14), Is.EqualTo(244.93));
+        }
+    }
+}
