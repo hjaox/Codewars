6aacb38a76b845dc2490a7e8b0c58a26f3bb9aac
Hector <hectorjana.o@gmail.com>
2023-11-09 12:59:21 +0000

    added SortNumbers kata

---
+++ CSharpUnitTests/8kyu/SortNumbersTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests._8kyu
+{
+    [TestFixture]
+    public class SortNumbersTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(SortNumbers.SortNumbersFunc(new int[] { 1, 2, 3, 10, 5 }), Is.Ordered.Ascending);
+        }
+
+        [Test]
+        public void ReturnsNewArray()
+        {
+            int[] testInput = { 5, 4, 3, 2, 1 };
+            Assert.That(SortNumbers.SortNumbersFunc(testInput), Is.Not.SameAs(testInput));
+        }
+    }
+}
