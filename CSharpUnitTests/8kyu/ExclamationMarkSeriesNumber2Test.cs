d3752fae40eb00a629603fef618f829311ab2844
Hector <hectorjana.o@gmail.com>
2023-11-09 20:14:58 +0000

    added ExclamationMarkSeriesNumber2

---
+++ CSharpUnitTests/8kyu/ExclamationMarkSeriesNumber2Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests._8kyu
+{
+    [TestFixture]
+    public class ExclamationMarkSeriesNumber2Test
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.That(ExclamationMarkSeriesNumber2.Remove("Hi!"), Is.EqualTo("Hi"));
+            Assert.That(ExclamationMarkSeriesNumber2.Remove("Hi!!!"), Is.EqualTo("Hi"));
+            Assert.That(ExclamationMarkSeriesNumber2.Remove("!Hi"), Is.EqualTo("!Hi"));
+            Assert.That(ExclamationMarkSeriesNumber2.Remove("Hi! Hi"), Is.EqualTo("Hi! Hi"));
+            Assert.That(ExclamationMarkSeriesNumber2.Remove("Hi"), Is.EqualTo("Hi"));
+        }
+    }
+}
