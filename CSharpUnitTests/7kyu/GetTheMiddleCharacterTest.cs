8a1d51593099fa97ea7b448c7e2bbb869a5f6c01
Hector <hectorjana.o@gmail.com>
2023-11-09 19:40:22 +0000

    added GetTheMiddleCharacter kata

--- CSharpUnitTests/7kyu/GetTheMiddleCharacterTest.cs
+++ CSharpUnitTests/7kyu/GetTheMiddleCharacterTest.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharp._7kyu;
 
 namespace CSharpUnitTests._7kyu
 {
-    internal class GetTheMiddleCharacterTest
+    [TestFixture]
+    public class GetTheMiddleCharacterTest
     {
+        [Test]
+        public void BasicTests()
+        {
+            ClassicAssert.AreEqual(GetTheMiddleCharacter.GetMiddle("test"), "es");
+            ClassicAssert.AreEqual(GetTheMiddleCharacter.GetMiddle("testing"), "t");
+            ClassicAssert.AreEqual(GetTheMiddleCharacter.GetMiddle("middle"), "dd");
+            ClassicAssert.AreEqual(GetTheMiddleCharacter.GetMiddle("A"), "A");
+        }
     }
 }
