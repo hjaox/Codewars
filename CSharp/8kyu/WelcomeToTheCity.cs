ec2dcc8183edaabd71b9cdbafaafe1131a85604b
Hector <hectorjana.o@gmail.com>
2023-11-10 20:47:38 +0000

    added WelcomeToTheCity kata

---
+++ CSharp/8kyu/WelcomeToTheCity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+https://www.codewars.com/kata/5302d846be2a9189af0001e4/csharp
+
+
+Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+Example:
+
+['John', 'Smith'], 'Phoenix', 'Arizona'
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+*/
+
+namespace CSharp._8kyu
+{
+    public class WelcomeToTheCity
+    {
+        public static string SayHello(string[] name, string city, string state)
+        {
+            return $"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!";
+        }
+    }
+}
---
+++ CSharpUnitTests/8kyu/WelcomeToTheCityTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests._8kyu
+{
+    [TestFixture]
+    public class WelcomeToTheCityTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            ClassicAssert.AreEqual("Hello, John Smith! Welcome to Phoenix, Arizona!", WelcomeToTheCity.SayHello(new string[] { "John", "Smith" }, "Phoenix", "Arizona"));
+        }
+    }
+}
