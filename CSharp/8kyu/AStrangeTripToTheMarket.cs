a9920bb490cce1ae5edc8c870a7815175ed56e33
Hector <hectorjana.o@gmail.com>
2023-11-12 15:06:47 +0000

    added AStrangTripToTheMarket kata

---
+++ CSharp/8kyu/AStrangeTripToTheMarket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CSharp._8kyu
+{
+    public class AStrangeTripToTheMarket
+    {
+        public static bool IsLockNessMonster(string sentence)
+        {
+            return Regex.IsMatch(sentence, @"tree fiddy|3\.50", RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ CSharpUnitTests/8kyu/AStrangeTripToTheMarketTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests._8kyu
+{
+    [TestFixture]
+    public class AStrangeTripToTheMarketTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            string testInput = "Your girlscout cookies are ready to ship. Your total comes to tree fiddy";
+            Assert.That(AStrangeTripToTheMarket.IsLockNessMonster(testInput), Is.True);
+
+            testInput = "Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance.";
+            Assert.That(AStrangeTripToTheMarket.IsLockNessMonster(testInput), Is.False);
+        }
+    }
+}
