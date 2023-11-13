abce522d954f2d1b89420b8b5878d238c4f09eb4
Hector <hectorjana.o@gmail.com>
2023-11-13 14:30:28 +0000

    added DivideAnd Conquer kata

---
+++ CSharp/7kyu/DivideAndConquer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+https://www.codewars.com/kata/57eaec5608fed543d6000021/csharp
+
+
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+Return as a number.
+*/
+
+namespace CSharp._7kyu
+{
+    public class DivideAndConquer
+    {
+        public static int DivCon(Object[] objArray)
+        {
+            return objArray.Sum(numObj => numObj is int ? (int)numObj : -Convert.ToInt32(numObj));
+        }
+    }
+}
