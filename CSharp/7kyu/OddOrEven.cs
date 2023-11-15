567617a3de44626a279e860378fa02263a845e43
Hector <hectorjana.o@gmail.com>
2023-11-15 13:53:25 +0000

    added OddOrEven kata

---
+++ CSharp/7kyu/OddOrEven.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+
+/*
+https://www.codewars.com/kata/5949481f86420f59480000e7/csharp
+
+
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+Have fun!
+*/
+
+namespace CSharp._7kyu;
+
+public class OddOrEven {
+    public static string OddOrEvenKata(int[] array) {
+        return array.Sum() % 2 == 0 ? "even" : "odd" ;
+    }
+}
