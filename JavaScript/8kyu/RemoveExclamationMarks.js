7aedb8b63685f8aa1d28f0348e557c853bca55fd
hjaox <hectorjana.o@gmail.com>
2023-12-03 12:55:43 +0000

    added previously solved katsa

---
+++ JavaScript/8kyu/RemoveExclamationMarks.js
@@ -0,0 +1,11 @@
+/*https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+function removeExclamationMarks(s) {
+    let a = s.replaceAll('!', '');
+    return a;        
+}
