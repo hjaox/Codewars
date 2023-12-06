39f0461acd140fbed1807ca496349a9aaa881dd3
Hector <hectorjana.o@gmail.com>
2023-12-06 20:10:29 +0000

    added previously solved katas

---
+++ JavaScript/8kyu/RemoveStringSpaces.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples:
+
+Input -> Output
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"
+*/
+
+function noSpace(x){
+    let a = x.split(' ');
+    for (i = 0; i < a.length; i++) {
+      if (a[i] === ' ') {
+        a.splice(i,1)
+      }
+    }
+    return a.join('');
+  }
