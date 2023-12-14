46f1391cb777e06e03a5388da6e72f6243832f9a
Hector <hectorjana.o@gmail.com>
2023-12-14 14:36:17 +0000

    added previously solved katas

---
+++ JavaScript/8kyu/RockPaperScissors.js
@@ -0,0 +1,23 @@
+/*
+https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+
+
+
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+
+const rps = (p1, p2) => {
+    return p1 === p2 ? `Draw!`
+    : p1 === 'rock' && p2 === 'scissors' ? 'Player 1 won!'
+    : p1 === 'rock' && p2 === 'paper' ? 'Player 2 won!'
+    : p1 === 'scissors' && p2 === 'rock' ? 'Player 2 won!'
+    : p1 === 'scissors' && p2 === 'paper' ? 'Player 1 won!'
+    : p1 === 'paper' && p2 === 'rock' ? 'Player 1 won!' : 'Player 2 won!'
+  };
