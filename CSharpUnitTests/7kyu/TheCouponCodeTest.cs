1c89b45289316c8c5cb089f181e1a25799436056
Hector <hectorjana.o@gmail.com>
2023-12-18 22:45:38 +0000

    added TheCouponCode kata

---
+++ CSharpUnitTests/7kyu/TheCouponCodeTest.cs
@@ -0,0 +1,15 @@
+﻿using CSharp._7kyu;
+
+namespace CSharpUnitTests._7kyu
+{
+    [TestFixture]
+    public class TheCouponCodeTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            ClassicAssert.AreEqual(true, TheCouponCode.CheckCoupon("123", "123", "September 5, 2014", "October 1, 2014"));
+            ClassicAssert.AreEqual(false, TheCouponCode.CheckCoupon("123a", "123", "September 5, 2014", "October 1, 2014"));
+        }
+    }
+}
