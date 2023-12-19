0a73b0d34638461ba502e746f555f772347a4964
hjaox <hectorjana.o@gmail.com>
2023-12-19 20:39:38 +0000

    added previously solved katas

---
+++ JavaScript/8kyu/IsItAPalindrome.js
@@ -0,0 +1,14 @@
+/*
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
+
+
+
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+*/
+
+function isPalindrome(x) {
+    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('')
+  }
