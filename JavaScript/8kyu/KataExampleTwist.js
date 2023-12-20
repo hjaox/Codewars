9e51efb1ad184d1b4e2521038ef42c1c60048cdc
hjaox <hectorjana.o@gmail.com>
2023-12-20 12:11:50 +0000

    added previously solved katas

---
+++ JavaScript/8kyu/KataExampleTwist.js
@@ -0,0 +1,11 @@
+/*
+https://www.codewars.com/kata/525c1a07bb6dda6944000031
+
+
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+*/
+
+var websites = Array.from({length: 1000}, x => 'codewars')
