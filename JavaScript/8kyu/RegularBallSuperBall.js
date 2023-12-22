c0bbd8a7b020b8a1d22fa0ba4bb184d76e4efe73
hjaox <hectorjana.o@gmail.com>
2023-12-22 10:12:36 +0000

    added previously solved katas

---
+++ JavaScript/8kyu/RegularBallSuperBall.js
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/53f0f358b9cb376eca001079
+
+
+
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+
+class Ball {
+    constructor(x) {
+      this.ballType = x || 'regular'
+    }
+  }
