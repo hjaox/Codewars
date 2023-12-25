8f70120bdf70eebe50f306a3e0643371610b9bcd
hjaox <hectorjana.o@gmail.com>
2023-12-25 15:54:08 +0000

    added previously solved katas

---
+++ JavaScript/6kyu/Meeting.js
@@ -0,0 +1,46 @@
+/*
+https://www.codewars.com/kata/59df2f8f08c6cec835000012
+
+
+
+
+John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.
+
+Notes
+You can see another examples in the "Sample tests".
+*/
+
+function meeting(s) {
+    // your code
+  let a = s.toUpperCase().split(';').map(x => x.replace(/(\w+):(\w+)/gi, '$2:$1')).sort().map(x => x.split(':'));
+  let b = {};
+  let ans = '';
+  
+  a.forEach(x => {
+    b[x[0]] ? b[x[0]].push(x[1]) : b[x[0]] = [x[1]];
+  })
+  
+  for(const a in b) {
+    if(b[a].length > 1) {
+      b[a].forEach(x => {
+        ans+=`(${a}, ${x})`;
+      })
+    } else {
+      ans+=`(${a}, ${b[a][0]})`
+    }
+  }
+  
+  return ans
+}
