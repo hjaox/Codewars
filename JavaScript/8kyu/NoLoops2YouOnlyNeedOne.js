f64800722e8ba55b31f1ab4932fbcd9ad01e5105
hjaox <hectorjana.o@gmail.com>
2023-12-25 15:17:39 +0000

    added previously solved katas

---
+++ JavaScript/8kyu/NoLoops2YouOnlyNeedOne.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce
+
+
+
+*** No Loops Allowed ***
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain n
+*/
+
+function check(a,x){
+    return a.includes(x)
+  };
