ecf5df39a430072fe61d312b8319828145026898
Hector <hectorjana.o@gmail.com>
2024-01-31 21:41:17 +0000

    added RotateForMax kata

---
+++ CSharp/7kyu/RotateForAMax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp._7kyu
+{
+    public class RotateForAMax
+    {
+        public static long MaxRot(long n)
+        {
+            string strNum = n.ToString();
+            long max = n;
+
+            for(int i = 0; i < strNum.Length - 1; i++)
+            {
+                strNum = strNum.Substring(0, i) + strNum.Substring(i + 1) + strNum[i];
+
+                if(Int64.Parse(strNum) > max) max = Int64.Parse(strNum);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ CSharpUnitTests/7kyu/RotateForAMaxTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._7kyu;
+
+namespace CSharpUnitTests._7kyu
+{
+    [TestFixture]
+    public class RotateForAMaxTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.That(RotateForAMax.MaxRot(38458215), Is.EqualTo(85821534));
+            Assert.That(RotateForAMax.MaxRot(195881031), Is.EqualTo(988103115));
+            Assert.That(RotateForAMax.MaxRot(896219342), Is.EqualTo(962193428));
+            Assert.That(RotateForAMax.MaxRot(69418307), Is.EqualTo(94183076));
+        }
+    }
+}
