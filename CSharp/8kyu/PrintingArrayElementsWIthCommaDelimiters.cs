fefe8ce87105da2babd9e0167f71a54717370f8f
Hector <hectorjana.o@gmail.com>
2024-01-31 22:14:43 +0000

    added PrintingArrayElementsWithCommaDelimiters kata

---
+++ CSharp/8kyu/PrintingArrayElementsWIthCommaDelimiters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp._8kyu
+{
+    public class PrintingArrayElementsWithCommaDelimiters
+    {
+        public static string PrintArray(object[] array)
+        {
+            return string.Join(",", array);
+        }
+    }
+}
---
+++ CSharpUnitTests/8kyu/PrintingArrayElementsWithCommaDelimitersTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp._8kyu;
+
+namespace CSharpUnitTests._8kyu
+{
+    [TestFixture]
+    public class PrintingArrayElementsWithCommaDelimitersTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.That(PrintingArrayElementsWithCommaDelimiters.PrintArray(new object[] { 2, 4, 5, 2}), Is.EqualTo("2,4,5,2"));
+        }
+    }
+}
