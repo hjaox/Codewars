60d99ba5f00db5872f16d91170153b182fc7a267
hjaox <hectorjana.o@gmail.com>
2024-02-16 12:44:08 +0000

    added GreetMe kata

---
+++ Python/7kyu/GreetMe.py
@@ -0,0 +1,14 @@
+'''
+https://www.codewars.com/kata/535474308bb336c9980006f2/python
+
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+'''
+
+def greet(name): 
+    return f"Hello {name[0].upper() + name.lower()[1:]}!"
